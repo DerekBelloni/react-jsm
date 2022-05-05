6fcdb5c65caf93b560199cd98cbfec0020059704
Derek <derekbelloni10@gmail.com>
2022-05-05 11:51:09 -0600

    Movies map to component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import './App.css';
-import MovieCard from './MovieCard';
+import MovieCard from "./MovieCard";
+
 
 //  API KEY: eea5807b
 
@@ -14,17 +15,17 @@ const movieOne = {
   "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
 }
 const App = () => {
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-
-    console.log(data);
+    console.log('[retrieving movies]', data.Search)
+    setMovies(data.Search);
   }
 
   useEffect(() => {
-    searchMovies('Spiderman');
+    searchMovies('Batman');
   }, []);
 
   return (
@@ -34,18 +35,34 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search for movies"
-          value="Superman"
+          value="Lord of the Rings"
           type="text"
           name=""
           id=""
           onChange={() => { }}
         />
-        /* Search Icon Here */
+
 
       </div>
-      <div className="container">
-        <MovieCard movieOne={movieOne} />
-      </div>
+
+
+
+
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movies) => (
+            <MovieCard movieOne={movies} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">No movies found</div>
+      )
+
+
+      }
+
+
     </div>
   );
 }
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 
-const MovieCard = (movieOne) => {
+const MovieCard = ({ movieOne }) => {
   return (
     <div className="movie">
       <div>
-        <p>{movieOne.Year}</p>
+        <p>2007</p>
       </div>
-
       <div>
         <img src={movieOne.Poster !== 'N/A' ? movieOne.Poster : 'https://via.placeholder.com/400'} alt={movieOne.Title} />
       </div>
-
       <div>
         <span>{movieOne.Type}</span>
         <h3>{movieOne.Title}</h3>
       </div>
-
     </div>
   )
 }
